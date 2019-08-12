62545a8b13fd123c25c0c7a0da8843ba805a61e6
Klaus Meyer <spam@klaus-meyer.net>
2019-08-12 22:03:26 +0200

    force tvdb2 gem to include www subdomain in image urls

--- lib/monkey_patches/tvdb2.rb
+++ lib/monkey_patches/tvdb2.rb
@@ -1,4 +1,10 @@
 module Tvdb2
+  class Client
+    def self.image_url(path)
+      URI::join("https://www.thetvdb.com/banners/", path).to_s
+    end
+  end
+
   class Series
     FIELDS = FIELDS + [:slug]
 
