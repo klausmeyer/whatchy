1ea50464fd7c15f2bc7012fc56d73945175c4682
Klaus Meyer <spam@klaus-meyer.net>
2019-08-12 14:26:44 +0200

    Setup Authentication with device gem

--- Gemfile
+++ Gemfile
@@ -42,4 +42,9 @@ end
 
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
-gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+# gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+
+# custom
+
+gem 'devise', '~> 4.6'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.find_or_initialize_by(email: 'admin@example.com').tap do |u|
+  u.password              = '12345678'
+  u.password_confirmation = u.password
+  u.save!
+end
