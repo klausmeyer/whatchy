02e111b40e89d889f7689a5101e47d3926e74415
Klaus Meyer <spam@klaus-meyer.net>
2019-08-13 11:48:16 +0200

    use banner cache host also for search view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-  def banner_url(show)
-    "#{Rails.application.config.x.banner_cache_host}#{show.image}"
+  def banner_url(path)
+    "#{Rails.application.config.x.banner_cache_host}#{path}"
   end
 end
