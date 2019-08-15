78577518ba94cf2976ec29d6f0d34a7abde148cf
Klaus Meyer <spam@klaus-meyer.net>
2019-08-15 13:49:21 +0200

    remove `after:` from migrations as it does not work with pgsql

--- db/migrate/20190815113801_add_block_sync_to_show.rb
+++ db/migrate/20190815113801_add_block_sync_to_show.rb
@@ -1,5 +1,5 @@
 class AddBlockSyncToShow < ActiveRecord::Migration[6.0]
   def change
-    add_column :shows, :block_sync, :boolean, null: false, default: false, after: :overview
+    add_column :shows, :block_sync, :boolean, null: false, default: false
   end
 end
