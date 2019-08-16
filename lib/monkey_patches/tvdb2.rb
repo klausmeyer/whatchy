49d01c93557f9c7ccaede37c45326a850f9bf7c8
Klaus Meyer <spam@klaus-meyer.net>
2019-08-16 21:48:18 +0200

    remove no longer needed monkey patch

--- lib/monkey_patches/tvdb2.rb
+++ lib/monkey_patches/tvdb2.rb
@@ -1,10 +1,4 @@
 module Tvdb2
-  class Client
-    def self.image_url(path)
-      URI::join("https://www.thetvdb.com/banners/", path).to_s
-    end
-  end
-
   class Series
     FIELDS = FIELDS + [:slug]
 
