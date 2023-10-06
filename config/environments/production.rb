414418478ebb88c6f3524a4688f39ba5c9d37f3e
Klaus Meyer <spam@klaus-meyer.net>
2023-10-06 21:08:01 +0200

    bring back default_url_options

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,4 +81,7 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
+  # custom
+  config.action_mailer.default_url_options = { host: ENV["DOMAIN"] }
 end
