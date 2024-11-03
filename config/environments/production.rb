b07ed9006b73cbd42fd42b300754f37f4727011a
Klaus Meyer <spam@klaus-meyer.net>
2024-11-03 10:45:29 +0100

    more

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,7 +87,10 @@ Rails.application.configure do
   config.active_support.report_deprecations = false
 
   # Do not dump schema after migrations.
-  # config.active_record.dump_schema_after_migration = false
+  config.active_record.dump_schema_after_migration = false
+
+  # Only use :id for inspections in production.
+  config.active_record.attributes_for_inspect = [ :id ]
 
   # Enable DNS rebinding protection and other `Host` header attacks.
   # config.hosts = [
