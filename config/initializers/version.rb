bb2c03a90d44fa74c3af7aba94fd55053a4ca90c
Klaus Meyer <spam@klaus-meyer.net>
2026-01-21 17:36:43 +0100

    release 0.2.0

--- config/initializers/version.rb
+++ config/initializers/version.rb
@@ -1,4 +1,4 @@
-Rails.application.config.x.version = "0.1.3"
+Rails.application.config.x.version = "0.2.0"
 
 # Docker Cloud will pass the git commit it is building the image from
 # See: https://docs.docker.com/docker-hub/builds/advanced/
