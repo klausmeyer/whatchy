d9c02a7d299d7b22dea074691460e62e4bbce688
Klaus Meyer <spam@klaus-meyer.net>
2026-06-15 15:55:15 +0200

    Send logs to Appsignal (#645)

---
+++ logging.rb
@@ -0,0 +1,5 @@
+if Rails.env.production?
+  appsignal_logger = Appsignal::Logger.new("rails")
+  appsignal_logger.broadcast_to(Rails.logger)
+  Rails.logger = ActiveSupport::TaggedLogging.new(appsignal_logger)
+end
