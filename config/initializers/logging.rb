10c69bece7fc76bd63bcd71a6cfdf08acca7c9a2
Klaus Meyer <spam@klaus-meyer.net>
2026-06-24 14:22:44 +0200

    remove appsignal logging (not working)

--- config/initializers/logging.rb
+++
@@ -1,5 +0,0 @@
-if Rails.env.production?
-  appsignal_logger = Appsignal::Logger.new("rails")
-  appsignal_logger.broadcast_to(Rails.logger)
-  Rails.logger = ActiveSupport::TaggedLogging.new(appsignal_logger)
-end
